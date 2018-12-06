80699202760df79faeec51fee85795b6794b32a7
Hackr <Hello@Camerons-MacBook-Pro.local>
2018-12-05 22:52:33 -0700

    Initial Commit

---
+++ coins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coins
+//
+//  Created by Hackr on 12/5/18.
+//  Copyright © 2018 Sugar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
